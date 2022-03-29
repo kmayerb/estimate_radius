f4f02ca29d0c25e05eceb3a34830e1d4a59eac11
kmayerb <kmayerb@gmail.com>
2022-03-29 11:28:43 -0700

    Add commandline execution

--- README.md
+++ README.md
@@ -2,5 +2,40 @@
 
 example of more intensive TCR radius estimation 
 
-
 ![image](https://user-images.githubusercontent.com/46639063/160673001-debde1b6-8a68-4194-a143-609f38a1b583.png)
+
+## requirements 
+
+Must have tcrdist3, olga, and tcrsampler installed with Britannova data downloaded.
+
+
+If not you can install that file easily:
+
+```python
+import tcrsampler 
+ts = tcrsampler.TCRsampler()
+ts.download_background_file('britanova_human_beta_t_cb.tsv.sampler.tsv.zip')
+```
+
+## Run from the commandline
+
+Estimate radius per clone using dense V-J gene matched synthetic neighborhoos
+
+arguments:
+  -h, --help          show this help message and exit
+  -p P                path to input file
+  -pout POUT          path to store outputs
+  -f F                filename clones file
+  -n_cpus N_CPUS      availale cpus to distribute
+  -n_runs N_RUNS      number of times to estimate ideal radius
+  -organism ORGANISM  human or mouse
+  -chains CHAINS      comma separated list of beta beta,alpha or alpha
+
+
+```bash
+python radii_estimation.py -p data -pout output -f dash_human.csv -n_cpus 2 -n_runs 10 -organism human -chains beta
+```
+
+## Recommedations 
+
+Try this out first on a small file and set n_runs to 3
--- radii_estimation.py
+++ radii_estimation.py
@@ -13,61 +13,85 @@ import os
 from tcrsampler.sampler import TCRsampler
 from tcrdist.background import get_stratified_gene_usage_frequency
 from routine import vj_tr
+import argparse
 
-p  = "data"
-pout = "output"
-f  = "dash_human.csv"
-# Set to max available to you
-n_cpus = 6
-# How many times do you wish to estimate, recommend doing this 10 times
-n_runs = 2
-organism = "human"
-chains = ['beta']
 
-fp = os.path.join(p,f)
-d  = pd.read_csv(fp)
+def main(p,
+         pout, 
+         f,
+         n_cpus, 
+         n_runs, 
+         organism, 
+         chains):
 
-if 'count' not in d.columns:
-    d['count'] = 1
+    fp = os.path.join(p,f)
+    d  = pd.read_csv(fp)
 
-du = d.groupby(['cdr3_b_aa','v_b_gene','j_b_gene']).size().sort_values(ascending = False).reset_index(drop = False)
-du.columns = ['cdr3_b_aa','v_b_gene','j_b_gene', 'count']
-print("VJ GENE USAGE FREQUENCY PER UNIQUE CLONE")
-print(du.groupby(['v_b_gene','j_b_gene']).size().sort_values(ascending = False))
+    if 'count' not in d.columns:
+        d['count'] = 1
 
-# Load a sampler for the purposes of estimating V and J gene usage 
-# frequency in real repertoires, which will be used later for weightings
+    du = d.groupby(['cdr3_b_aa','v_b_gene','j_b_gene']).size().sort_values(ascending = False).reset_index(drop = False)
+    du.columns = ['cdr3_b_aa','v_b_gene','j_b_gene', 'count']
+    print("VJ GENE USAGE FREQUENCY PER UNIQUE CLONE")
+    print(du.groupby(['v_b_gene','j_b_gene']).size().sort_values(ascending = False))
 
-ts = TCRsampler(default_background = 'britanova_human_beta_t_cb.tsv.sampler.tsv')
-ts = get_stratified_gene_usage_frequency(ts = ts, replace = True) 
+    # Load a sampler for the purposes of estimating V and J gene usage 
+    # frequency in real repertoires, which will be used later for weightings
 
-# Create a list of Data.Frames each grouped by v and j gene usage 
-gs = [g for i,g in d.groupby(['v_b_gene','j_b_gene'])]
-fs = list()
-for sim in range(n_runs):
-    print(sim)
-    # Use multiple cpus to speed this up, the actual work is being done in the routine.py script
-    y2 = parmap.map(vj_tr, gs, ts = ts, organism = organism, chains = chains, pm_processes = n_cpus, pm_pbar = True)
-    y_df = pd.concat(y2)
-    fs.append(os.path.join(pout, f"{f}_radii_estimate_{sim}.tsv"))
-    y_df.to_csv(os.path.join(pout, f"{f}_radii_estimate_{sim}.tsv"), sep = "\t", index = False)
+    ts = TCRsampler(default_background = 'britanova_human_beta_t_cb.tsv.sampler.tsv')
+    ts = get_stratified_gene_usage_frequency(ts = ts, replace = True) 
 
-# After thsi stage all of the computation is done. 
-# we just reread all of the saved file and compute summary statistics 
-# on radii estimates per clone.
+    # Create a list of Data.Frames each grouped by v and j gene usage 
+    gs = [g for i,g in d.groupby(['v_b_gene','j_b_gene'])]
+    fs = list()
+    for sim in range(n_runs):
+        print(sim)
+        # Use multiple cpus to speed this up, the actual work is being done in the routine.py script
+        y2 = parmap.map(vj_tr, gs, ts = ts, organism = organism, chains = chains, pm_processes = n_cpus, pm_pbar = True)
+        y_df = pd.concat(y2)
+        fs.append(os.path.join(pout, f"{f}_radii_estimate_{sim}.tsv"))
+        y_df.to_csv(os.path.join(pout, f"{f}_radii_estimate_{sim}.tsv"), sep = "\t", index = False)
 
-#fs = [f for f in os.listdir(pout) if f.find('radii_estimate_') != -1]
-print(fs)
-dfs = [pd.read_csv(os.path.join(pout,f), sep= "\t") for f in fs]
-dfall = pd.concat(dfs)
+    # After thsi stage all of the computation is done. 
+    # we just reread all of the saved file and compute summary statistics 
+    # on radii estimates per clone.
 
-x = dfall[['cdr3_b_aa','v_b_gene','j_b_gene','radi']].\
-    groupby(['cdr3_b_aa','v_b_gene','j_b_gene']).\
-    agg(['min','mean','median','max','std']).\
-    reset_index()
+    #fs = [f for f in os.listdir(pout) if f.find('radii_estimate_') != -1]
+    print(fs)
+    dfs = [pd.read_csv(os.path.join(pout,f), sep= "\t") for f in fs]
+    dfall = pd.concat(dfs)
 
-mi  = x.columns
-ind = pd.Index([e[0] +"_"+ e[1] if e[0] not in ['cdr3_b_aa','v_b_gene','j_b_gene'] else e[0] for e in mi.tolist() ])
-x.columns = ind
-x['radi_range']=x['radi_max']-x['radi_min']
-x.to_csv(os.path.join(pout,f'{f}_radii_estimates_across_{n_runs}_runs.tsv'),sep = "\t", index = False)
+    x = dfall[['cdr3_b_aa','v_b_gene','j_b_gene','radi']].\
+        groupby(['cdr3_b_aa','v_b_gene','j_b_gene']).\
+        agg(['min','mean','median','max','std']).\
+        reset_index()
+
+    mi  = x.columns
+    ind = pd.Index([e[0] +"_"+ e[1] if e[0] not in ['cdr3_b_aa','v_b_gene','j_b_gene'] else e[0] for e in mi.tolist() ])
+    x.columns = ind
+    x['radi_range']=x['radi_max']-x['radi_min']
+    x.to_csv(os.path.join(pout,f'{f}_radii_estimates_across_{n_runs}_runs.tsv'),sep = "\t", index = False)
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='Estimate radius per clone using dense V-J gene matched synthetic neighborhoos')
+    parser.add_argument('-p',        help='path to input file', required=True)
+    parser.add_argument('-pout',     help='path to store outputs', required=True)
+    parser.add_argument('-f',        help='filename clones file', required=True)
+    parser.add_argument('-n_cpus',   help='availale cpus to distribute', required=True)
+    parser.add_argument('-n_runs',   help='number of times to estimate ideal radius', required=True)
+    parser.add_argument('-organism', help='human or mouse', required=True)
+    parser.add_argument('-chains',   help='comma separated list of beta  beta,alpha or alpha' , required=False, default = "beta")  
+    args = parser.parse_args()
+    print(vars(parser.parse_args()))
+    
+    main(
+        p        = args.p, #"data"
+        pout     = args.pout, #"output"
+        f        = args.f, #"dash_human.csv"
+        n_cpus   = int(args.n_cpus),#6
+        n_runs   = int(args.n_runs), #10
+        organism = args.organism ,#"human"
+        chains   = args.chains.split(",") )
+    """
+    python radii_estimation.py -p data -pout output -f dash_human.csv -n_cpus 2 -n_runs 10 -organism human -chains beta
+    """
